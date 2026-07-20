4fc04d86a0fa568a0a1a53f0054241ddc9ad52b3
unknown <naboharik@gmail.com>
2026-07-20 12:55:53 +0530

    new file

---
+++ taxonomy.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import statsmodels.api as sm
+import patsy as pt
+
+
+import numpy as np
+
+def insert_attack(attack):
+    return 0
+
+def insert_coding(coder,category):
+    return 0
+
+def get_rating_conflicts(attack):
+    return 0
+
+def correct_conflict(attack, coder1, coder2, category):
+    return 0
+
+def show_taxonomy():
+    return  0
+
+def cal_reliability(rator1, rator2):
+    return 0
+
+
+
+print("dsdfdfd")
